ae7082bfd241fe4287ce3d75a935e72773c4bdd3
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-08-28 02:40:28 +0300

    Column, Row and Box

--- app/src/main/java/com/example/betrbetatools/MainActivity.kt
+++ app/src/main/java/com/example/betrbetatools/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.betrbetatools
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -38,13 +39,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+
+    //Column, Row, Box
+    Column {
+        Text(
             text = "Hello $name!",
             modifier = modifier,
             color = Color.Magenta,
             fontSize = 25.sp,
             fontWeight = FontWeight.Bold
-    )
+        )
+        Text(
+            text = "Hello World!",
+            modifier = modifier,
+            color = Color.Magenta,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+
 }
 
 @Preview(showBackground = true) // Emulator shows that what is in here
